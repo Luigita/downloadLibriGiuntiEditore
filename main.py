459e870e8742d275fe1605e006541488ce3a7199
Luigita <98612942+Luigita@users.noreply.github.com>
2023-09-27 16:35:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import requests
 
+page = 1
+total_pages = 120
 
+print("Page " + format(page, "03d") + " done")
 
-
-inter = 1
-
-
-print(format(inter, "03d"))
-
-
-while inter <= 120:
-	response = requests.get("https://static.dbookeasy.giuntiscuola.it/progetti/progetto_33/book_518/pages/1665673802" + format(inter, "03d") + ".png")
+while page <= total_pages:
+	response = requests.get("https://static.dbookeasy.giuntiscuola.it/progetti/progetto_33/book_518/pages/1665673802" + format(page, "03d") + ".png")
 	if response.status_code == 200:
-		with open(r"Pages\Page " + format(inter) + ".PNG", "wb") as f:
+		with open(r"Pages\Page " + format(page) + ".PNG", "wb") as f:
 			f.write(response.content)
-			print("Pagina " + format(inter, "03d") + " scaricata")
-	inter = inter + 1
+			print("Pagina " + format(page, "03d") + " scaricata")
+	page = page + 1
